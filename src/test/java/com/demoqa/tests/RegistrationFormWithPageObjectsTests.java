4181126c65471e207056bc021167048758e4a7bb
renathi <griffmbt@gmail.com>
2022-09-19 11:08:56 +0300

    moved the variables from RegistrationFormWithPageObjectsTests to TestDate

--- src/test/java/com/demoqa/tests/RegistrationFormWithPageObjectsTests.java
+++ src/test/java/com/demoqa/tests/RegistrationFormWithPageObjectsTests.java
@@ -2,50 +2,12 @@ package com.demoqa.tests;
 
 import com.demoqa.pages.RegistrationFormPage;
 
-import com.github.javafaker.Faker;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Locale;
-
 import static com.demoqa.tests.TestData.*;
-import static com.demoqa.utils.RandomUtils.*;
-import static java.lang.String.format;
 
 public class RegistrationFormWithPageObjectsTests extends TestBase {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
-    Faker faker = new Faker(new Locale("en"));
-
-    String firstName,
-            lastName,
-            userFullName,
-            userEmail,
-            gender,
-            userNumber,
-            day,
-            month,
-            year,
-            userDateOfBirth,
-            hobby,
-            currentAddress,
-            userStateAndCity;
-
-    @BeforeEach
-    void prepareTestData() {
-        firstName = faker.name().firstName();
-        lastName = faker.name().lastName();
-        userFullName = format("%s %s", firstName, lastName);
-        userEmail = faker.internet().emailAddress();
-        gender = getRandomGender();
-        userNumber = faker.phoneNumber().subscriberNumber(10);
-        day = faker.number().numberBetween(1, 30) + "";
-        month = getRandomMonth();
-        year = faker.number().numberBetween(1900, 2010) + "";
-        userDateOfBirth = format("%s %s,%s", day, month, year);
-        hobby = getRandomHobby();
-        currentAddress = faker.address().fullAddress();
-        userStateAndCity = format("%s %s", userState, userCity);
-    }
 
     @Test
     void fillFormTest() {
--- src/test/java/com/demoqa/tests/TestData.java
+++ src/test/java/com/demoqa/tests/TestData.java
@@ -1,12 +1,32 @@
 package com.demoqa.tests;
 
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+import static com.demoqa.utils.RandomUtils.*;
+import static java.lang.String.format;
 
 public class TestData {
+    static Faker faker = new Faker(new Locale("en"));
     static String
+            firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            userFullName = format("%s %s", firstName, lastName),
+            userEmail = faker.internet().emailAddress(),
+            gender = getRandomGender(),
+            userNumber = faker.phoneNumber().subscriberNumber(10),
+            day = faker.number().numberBetween(1, 30) + "",
+            month = getRandomMonth(),
+            year = faker.number().numberBetween(1900, 2010) + "",
+            userDateOfBirth = format("%s %s,%s", day, month, year),
+            hobby = getRandomHobby(),
+            currentAddress = faker.address().fullAddress(),
             subjects = "Math",
             pictureName = "cat.jpeg",
             uploadPictureSrc = "src/test/resources/cat.jpeg",
             userState = "NCR",
-            userCity = "Delhi";
+            userCity = "Delhi",
+            userStateAndCity = format("%s %s", userState, userCity);
 
 }
